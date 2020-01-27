70776d03e48ff26d959392759307e560b331a356
Nimrod Dolev <nimrodavid@gmail.com>
2020-01-27 08:41:17 +0000

    chore: @probot/serverless-lambda version upgrade

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-const { serverless } = require('@probot/serverless-lambda')
-const appFn = require('./handler')
-var app = serverless(appFn)
-module.exports.probot = function (request: any, context: any, callback: any) {
-    if (request.isBase64Encoded) {
-        request.body = Buffer.from(request.body, "base64").toString('utf8');
-    }
-    app(request, context, callback);
-}
+const { serverless } = require('@probot/serverless-lambda');
+const app = require('./handler');
+module.exports.probot = serverless(app);
